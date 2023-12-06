126560f3bc7ac57ac676cee962e39e3f5bf42cec
Kevinalexander14 <kevinalexander.kdca@gmail.com>
2023-12-06 09:16:12 +0700

    Revisi

---
+++ Tugas FE.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginBtn = document.getElementById('login-btn');
+    const registerBtn = document.getElementById('register-btn');
+  
+
+    loginBtn.addEventListener('click', function() {
+
+      alert('Implement your login logic');
+    });
+  
+    registerBtn.addEventListener('click', function() {
+      alert('Implement your register logic');
+    });
+  });
+  
